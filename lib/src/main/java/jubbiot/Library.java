a45c348e0d448c1b5081e0b413b14500f566445c
Massimo Santini <santini@di.unimi.it>
2024-02-26 14:30:51 +0100

    First commit

---
+++ lib/src/main/java/jubbiot/Library.java
@@ -0,0 +1,15 @@
+package jubbiot;
+
+/**
+ * The class Library.
+ */
+public class Library {
+    /**
+     * A nice method.
+     * 
+     * @return {@code true}
+     */
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
